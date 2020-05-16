2dd1abda3753507a1bda79b4a0b9b0e6d4373911
anastasiyaNasevichLeverx <anastasiya.nasevich@leverx.com>
2020-05-16 19:31:07 +0300

    feat: add babel config with tagets

---
+++ build/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class App {
+  constructor() {
+    this.run = () => {
+      console.log('dfdf');
+    };
+  }
+
+}
+
+const app = new App();
+app.run();
---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+
+class App {
+
+    constructor() {
+            this.run =() => {
+                console.log('dfdf')
+            }
+    }
+
+}
+
+const app = new App()
+app.run()
