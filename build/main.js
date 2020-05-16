1d0743191acc350deb36b1194fcfc6467c18f32e
anastasiyaNasevichLeverx <anastasiya.nasevich@leverx.com>
2020-05-16 20:37:37 +0300

    feat: add core-js polyfills

--- build/main.js
+++ build/main.js
@@ -1,13 +1,49 @@
 "use strict";
 
-class App {
-  constructor() {
-    this.run = () => {
-      console.log('dfdf');
-    };
-  }
+require("core-js/modules/es.array.flat");
 
-}
+require("core-js/modules/es.array.unscopables.flat");
 
-const app = new App();
-app.run();
+require("core-js/modules/es.object.to-string");
+
+require("core-js/modules/es.promise");
+
+require("core-js/modules/es.promise.finally");
+
+require("regenerator-runtime/runtime");
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var App = function App() {
+  _classCallCheck(this, App);
+
+  _defineProperty(this, "run", /*#__PURE__*/_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    var state;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            state = {};
+            console.log(state === null || state === void 0 ? void 0 : state.isActive);
+            console.log([[1, 2], 3].flat());
+
+          case 3:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  })));
+};
+
+var app = new App();
+app.run().then(function () {}).finally(function () {
+  return console.log('done');
+}); // download core-js library for adding polyfills for new features, add it in .babelrc
+//
--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-
-
 class App {
-
-    constructor() {
-            this.run =() => {
-                console.log('dfdf')
-            }
+    run = async () => {
+        const state = {}
+        console.log(state?.isActive)
+        console.log([[ 1, 2 ], 3].flat())
     }
-
 }
 
 const app = new App()
-app.run()
+app.run().then(() => {}).finally(() => console.log('done'))
+
+
+// download core-js library for adding polyfills for new features, add it in .babelrc
+//
